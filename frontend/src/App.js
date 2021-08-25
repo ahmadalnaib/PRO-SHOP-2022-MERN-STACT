c15ba6eb111a38137473a93fcabad49e6a2302cc
ahmedhabeeb73 <alnaib888@gmail.com>
2021-08-25 15:54:16 +0200

    add react in front end and add navbar and bootstrap and fontawe

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+import {Container} from 'react-bootstrap';
+
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+        <h1>Welcome to Proshop</h1>
+
+        </Container>
+      </main>
+    <Footer/>
+    </>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Container,Navbar,Nav} from 'react-bootstrap';
+
+const Header = () => {
+  return <header>
+   
+
+<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Container>
+  <Navbar.Brand href="/">ProShop</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="me-auto"></Nav>
+    <Nav>
+    <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i> Cart</Nav.Link>
+     <Nav.Link href="/login"><i className="fas fa-user"></i> Sign in</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+  </Container>
+</Navbar>
+  </header>;
+};
+
+export default Header;
